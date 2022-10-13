f4d02dfccefda0c0ec41271e425e4352cb3020e8
Wiwok <emil.dechenaud@gmail.com>
2022-10-13 15:47:47 +0200

    Update date verification

--- Functions/RateEvening.ts
+++ Functions/RateEvening.ts
@@ -6,6 +6,16 @@ function rateEvening(req: any, res: any, localPath: string) {
 	const month = parseInt(req.params.month);
 	const day = parseInt(req.params.day);
 
+	if (isNaN(month) || month > 12 || month < 1) {
+		res.status(400).json({ error: 1, msg: 'Invalid month' });
+		return;
+	}
+
+	if (isNaN(day) || day > 31 || day < 1) {
+		res.status(400).json({ error: 1, msg: 'Invalid day' });
+		return;
+	}
+
 	if (!fs.existsSync(localPath + `menus/${month}/${day}.json`)) {
 		res.status(404).json({ error: 1, msg: 'Menu not found' });
 		return;
